95278ecf9c9c6e6db933e6e3d21e791f5dc7df2f
XRuiz9 <xdruiz92@gmail.com>
2019-06-17 00:39:06 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(args)
+  	meep = Network.create(call_letters: args[:call_letters])
+  	self.network_id = meep.id
+  	# binding.pry
+  	meep
+  end
+
 end
